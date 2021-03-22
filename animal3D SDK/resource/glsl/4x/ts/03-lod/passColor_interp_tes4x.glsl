6f663eef2e4b27b4948aad24d919d3fd61ba7121
Nick <nicholas.preis@mymail.champlain.edu>
2021-03-22 14:19:16 -0400

    Code from class.

--- animal3D SDK/resource/glsl/4x/ts/03-lod/passColor_interp_tes4x.glsl
+++ animal3D SDK/resource/glsl/4x/ts/03-lod/passColor_interp_tes4x.glsl
@@ -31,11 +31,37 @@
 
 layout (isolines, equal_spacing) in;
 
+uniform ubCurve
+{
+	vec4 uCurveWaypoint[32];
+	vec4 uCurveTangent[32];
+};
+uniform int uCount;
+
 uniform mat4 uP;
 
 out vec4 vColor;
 
 void main()
 {
-	
+	//gl_TessCoord for isolines:
+	//	[0] = How far along line from 0-1 (inclusive)
+	//	[1] = which line from a 0-1 value (never actually 1)
+
+	// In this example:
+	// gl_TessCoord[0] = interpolation parameter
+	// gl_TessCoord[1] = 0
+	int i0 = gl_PrimitiveID; // Current
+	int i1 = (i0 + 1) % uCount; // Destination
+	float u = gl_TessCoord[0];
+
+	//vec4 p = vec4(gl_TessCoord[0], 0.0, -1.0, 1.0);
+	vec4 p = mix(
+		uCurveWaypoint[i0],
+		uCurveWaypoint[i1],
+		u
+	);
+	gl_Position = uP * p;
+
+	vColor = vec4(0.5, 0.0, 1.0, 1.0);
 }
